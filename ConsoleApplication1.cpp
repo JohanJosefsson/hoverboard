2515e6918367c0a542f318bc98c3ad99ddff189d
Johan Josefsson <johan.josefsson@rocketmail.com>
2018-06-17 20:59:08 +0200

    My first commit

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+	std::cout << "Tjena" << std::endl;
+	int i;
+	std::cin >> i;
+	std::string s;
+	std::cin >> s;
+    return 0;
+}
+
